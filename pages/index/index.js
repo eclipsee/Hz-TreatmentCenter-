0dbe589221f0900d3fc40b156525231f90b92b3e
jun.wang <jun.wang@ximalaya.com>
2019-06-13 14:58:04 +0800

    提交图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-import { get, post } from '../../requests/request'
+// import { get, post } from '../../requests/request'
 
 const app = getApp()
 
@@ -6,15 +6,64 @@ Page({
   data: {
     loading: false,
     disabled: false,
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    text: 0
+    emotionList: []
   },
 
-  onLoad: function() {},
+  onLoad: function() {
+    this.drawEmotion()
+    this.regularDraw()
+  },
 
-  drawEmotion() {},
+  regularDraw() {
+    setInterval(() => {
+      this.drawEmotion()
+    }, 10000)
+  },
+
+  drawEmotion() {
+    const data = []
+    for (let index = 0; index < 5; index++) {
+      data.push(Math.ceil(Math.random(1, 12) * 10))
+    }
+    this.setData({
+      emotionList: data
+    })
+
+    this.drawAnimation()
+  },
+
+  drawAnimation() {
+    var animation = wx.createAnimation({
+      duration: 10000,
+      timingFunction: 'ease',
+      delay: 0
+    })
+
+    animation
+      .opacity(0)
+      .left(10)
+      .bottom(400)
+      .scale(3)
+      .step()
+
+    var animation1 = wx.createAnimation({
+      duration: 10000,
+      timingFunction: 'ease',
+      delay: 0
+    })
+
+    animation1
+      .opacity(0)
+      .right(10)
+      .bottom(400)
+      .scale(2)
+      .step()
+
+    this.setData({
+      animationData: animation.export(),
+      animationData1: animation1.export()
+    })
+  },
 
   // 跳转至情绪日历
   onClickCalendar: function() {
