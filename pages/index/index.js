52fd30887a15a394f7f3721b7fadceed71b0c2de
jun.wang <jun.wang@ximalaya.com>
2019-06-13 17:15:18 +0800

    提交图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,53 +16,93 @@ Page({
 
   regularDraw() {
     setInterval(() => {
-      this.drawEmotion()
-    }, 10000)
+      this.setData({
+        emotionList: []
+      })
+      const animationData = this.drawStart('left').export()
+      const animationData1 = this.drawStart('right').export()
+      this.setData({
+        animationData,
+        animationData1
+      })
+      setTimeout(() => {
+        this.drawEmotion()
+      }, 0)
+    }, 5000)
   },
 
   drawEmotion() {
     const data = []
-    for (let index = 0; index < 5; index++) {
-      data.push(Math.ceil(Math.random(1, 12) * 10))
+    for (let index = 0; index < 6; index++) {
+      const temp = Math.ceil(Math.random(1, 12) * 10)
+      data.push({
+        image: temp,
+        key: Math.ceil(Math.random(10, 120) * 100),
+        viewClass: index % 2 ? 'emotion' : 'emotion1',
+        imageClass: `image${temp % 4}`
+      })
     }
     this.setData({
       emotionList: data
     })
 
-    this.drawAnimation()
+    const animationData = this.drawAnimation('left').export()
+    const animationData1 = this.drawAnimation('right').export()
+
+    this.setData({ animationData, animationData1 })
   },
 
-  drawAnimation() {
+  drawAnimation(type) {
     var animation = wx.createAnimation({
-      duration: 10000,
+      duration: 5000,
+      timingFunction: 'ease',
+      delay: 0
+    })
+
+    const bottom = Math.ceil((3 + Math.random() * (5 + 1 - 3)) * 100)
+    const scale = 1.5 + Math.random() * (3 + 1 - 1)
+
+    animation
+      .opacity(0)
+      [type](10)
+      .bottom(bottom)
+      .scale(scale)
+      .step()
+
+    return animation
+  },
+
+  drawStart(type) {
+    var animation = wx.createAnimation({
+      duration: 0,
+      timingFunction: 'ease',
+      delay: 0
+    })
+
+    animation
+      .opacity(1)
+      [type]('50%')
+      .bottom(200)
+      .scale(1)
+      .step()
+
+    return animation
+  },
+
+  drawCatch() {
+    var animation = wx.createAnimation({
+      duration: 5000,
       timingFunction: 'ease',
       delay: 0
     })
 
     animation
       .opacity(0)
-      .left(10)
-      .bottom(400)
-      .scale(3)
+      .bottom(350)
+      .scale(350)
       .step()
 
-    var animation1 = wx.createAnimation({
-      duration: 10000,
-      timingFunction: 'ease',
-      delay: 0
-    })
-
-    animation1
-      .opacity(0)
-      .right(10)
-      .bottom(400)
-      .scale(2)
-      .step()
-
-    this.setData({
-      animationData: animation.export(),
-      animationData1: animation1.export()
-    })
+    return animation
   },
 
   // 跳转至情绪日历
@@ -72,7 +112,5 @@ Page({
     })
   },
 
-  onClickCatch: function() {
-    console.log('....打捞泡泡')
-  }
+  onClickCatch: function() {}
 })
