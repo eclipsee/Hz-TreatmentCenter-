a04500c3e22c2bcf8a2f1569f747595b46be649c
lizifen <zifenli@gmail.com>
2019-06-14 14:11:39 +0800

    replies 切图

--- components/replies/replies.js
+++ components/replies/replies.js
@@ -4,13 +4,20 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    comments: {
+      type: Array,
+      default: [],
+    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+
+  },
+
+  ready() {
 
   },
 
--- pages/bubbleDetail/index.js
+++ pages/bubbleDetail/index.js
@@ -1,4 +1,6 @@
 // pages/bubbleDetail/index.js
+const testUrl = 'http://yss.yisell.com/yisell/ybys2018050819052088/sound/yisell_sound_2014031622091974505_88366.mp3';
+const testAvatar = 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg';
 Page({
 
   /**
@@ -7,14 +9,19 @@ Page({
   data: {
     user: {
       username: 'lizifen',
-      avatar: 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg',
+      avatar: testAvatar,
     },
     bubble: {
       title: '分手求安慰',
-      url: 'http://yss.yisell.com/yisell/ybys2018050819052088/sound/yisell_sound_2014031622091974505_88366.mp3',
+      url: testUrl,
       duration: 10,
       type: 1, // 1, 开心，2，悲伤，3，愤怒，4，语音回复，5，治愈音
     },
+    comments: [
+      { user: { username: 'lizifen', avatar: testAvatar }, bubble: { url: testUrl, duration: 10, comment_type: 1 } },
+      { user: { username: 'lizifen', avatar: testAvatar }, bubble: { url: testUrl, duration: 10, comment_type: 2 } },
+      { user: { username: 'lizifen', avatar: testAvatar }, isSelf: true, bubble: { url: testUrl, duration: 10, comment_type: 3 } },
+    ],
   },
 
   /**
