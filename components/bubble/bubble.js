c2ce24cd226792cb63baf50c11cd7e97725c3911
lizifen <zifenli@gmail.com>
2019-06-14 11:52:53 +0800

    添加bubble

--- components/bubble/bubble.js
+++ components/bubble/bubble.js
@@ -5,8 +5,8 @@ Component({
    */
   properties: {
     size: {
-      type: Number,
-      value: 50,
+      type: String,
+      value: 'small',
     },
     bubble: {
       type: Object,
@@ -18,13 +18,40 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing: false,
+    time: 0,
+    timer: null,
+  },
 
+  ready() {
+    this.audioCtx = wx.createInnerAudioContext();
+    this.audioCtx.src = this.data.bubble.url;
+    this.audioCtx.onEnded(() => {
+      clearInterval(this.data.timer);
+      this.setData({ playing: false, time: 0 });
+    });
+  },
+  
+  detached() {
+    clearInterval(this.data.timer);
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    togglePlay() {
+      let timer = null;
+      if (this.data.playing) {
+        this.audioCtx.pause();
+        clearInterval(this.data.timer);
+      } else {
+        this.audioCtx.play();
+        timer = setInterval(() => {
+          this.setData({ time: this.data.time + 1 });
+        }, 1000);
+      }
+      this.setData({ playing: !this.data.playing, timer });
+    },
   },
 });
