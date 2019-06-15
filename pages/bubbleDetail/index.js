756bd90661b6f32f7fd529d132c579fc894240ed
lizifen <zifenli@gmail.com>
2019-06-15 10:58:00 +0800

    添加swiper

--- pages/bubbleDetail/index.js
+++ pages/bubbleDetail/index.js
@@ -1,6 +1,7 @@
 // pages/bubbleDetail/index.js
 const testUrl = 'http://yss.yisell.com/yisell/ybys2018050819052088/sound/yisell_sound_2014031622091974505_88366.mp3';
 const testAvatar = 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg';
+
 Page({
 
   /**
@@ -22,6 +23,26 @@ Page({
       { user: { username: 'lizifen', avatar: testAvatar }, bubble: { url: testUrl, duration: 10, comment_type: 2 } },
       { user: { username: 'lizifen', avatar: testAvatar }, isSelf: true, bubble: { url: testUrl, duration: 10, comment_type: 3 } },
     ],
+    replyType: 0,
+    replyTypes: [
+      { name: '语音', value: 1, icon: '../../img/1.png' },
+      { name: '歌曲', value: 2, icon: '../../img/2.png' },
+      { name: '段子', value: 3, icon: '../../img/3.png' },
+      { name: '治愈音', value: 4, icon: '../../img/4.png' },
+    ],
+  },
+
+  changeType(e) {
+    const { type } = e.currentTarget.dataset;
+    if (type === 'more') {
+      wx.showModal({
+        title: '提示',
+        content: '此处功能可以无限拓展',
+        success() {},
+      });
+    } else {
+      this.setData({ replyType: type - 1 });
+    }
   },
 
   /**
