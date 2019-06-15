d890d0f2a683018749906b3db0ccc24526446b87
lizifen <zifenli@gmail.com>
2019-06-15 20:28:58 +0800

    joke 数据

--- pages/bubbleDetail/index.js
+++ pages/bubbleDetail/index.js
@@ -1,4 +1,5 @@
-// pages/bubbleDetail/index.js
+import jokes from '../../data/joke';
+
 const testUrl = 'http://yss.yisell.com/yisell/ybys2018050819052088/sound/yisell_sound_2014031622091974505_88366.mp3';
 const testAvatar = 'http://img.wxcha.com/file/201807/13/9bbc369f6e.jpg';
 
@@ -34,15 +35,20 @@ Page({
     ],
     library: {
       music: [{ name: '分手快乐', title: '你是TA永远得不到的爸爸', content: '分手快乐祝你快乐你可以找到更好的' }],
-      joke: [{ name: '这个笑话很好笑', title: '你笑了吗？', content: '哈哈哈哈哈哈哈哈哈哈哈哈' }],
+      joke: jokes,
       cure: [{ name: '小海豚治愈不开心', title: '花莲海豚叫声', url: 'http://yss.yisell.com/yisell/ybys2018050819052088/sound/yisell_sound_2014031622091974505_88366.mp3' }],
     },
   },
 
   randomReplay() {
     if (this.data.replyType === 'voice') return {};
+  
     const options = this.data.library[this.data.replyType];
-    return options[0];
+    
+    const randomseed = options.length;
+    const index = Math.ceil(1 + Math.random() * randomseed);
+
+    this.setData({ currentReplay: options[index] });
   },
 
   changeType(e) {
@@ -57,7 +63,7 @@ Page({
       this.setData({ isExpand: false });
     } else {
       this.setData({ replyType: type, isExpand: true }, () => {
-        this.setData({ currentReplay: this.randomReplay() });
+        this.randomReplay();
       });
     }
   },
