15e5a7ce9c5caef11b9ad909bfea1534ada11961
lizifen <zifenli@gmail.com>
2019-06-16 10:13:25 +0800

    连接云服务

--- app.js
+++ app.js
@@ -34,9 +34,32 @@ App({
         }
       }
     })
+     
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+        env: 'treatmentcenter-620d83'
+      })
+
+      // 调用云函数
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log('[云函数] [login] user openid: ', res.result.openid)
+          this.globalData.openid = res.result.openid
+        },
+        fail: err => {
+          console.error('[云函数] [login] 调用失败', err)
+        }
+      })
+    }
   },
   globalData: {
     userInfo: null,
-    store:store
+    store: store,
+    openid: '',
   }
 })
